5e880fbd7e88c9c76bd71ea5c102b059de432d3a
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-03-09 15:06:57 +0000

    3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -5,6 +5,7 @@ This script starts a Flask web application with three routes
 - '/' displays "Hello HBNB!"
 - '/hbnb' displays "HBNB"
 - '/c/<text>' displays "C "
+- '/python/<text>' displays "Python "
 """
 from flask import Flask
 app = Flask(__name__)
@@ -27,6 +28,14 @@ def display_c(text):
     """Displays 'C ' followed by the value of the text variable
        (replace underscore _ symbols with a space )"""
     return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_python(text):
+    """Displays 'Python ' followed by the value of the text variable
+       (replace underscore _ symbols with a space )"""
+    return 'Python {}'.format(text.replace('_', ' '))
 
 
 if __name__ == '__main__':
