8f92e8e5c10ee2701c453536fc0da0291fe27067
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-03-10 12:13:05 +0000

    Updated issues with spacing

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -66,6 +66,6 @@ class FileStorage:
                 del self.__objects[key]
 
      def close(self):
-        """Call reload() method for deserializing the JSON file to objects"""
-        self.reload()
+         """Call reload() method for deserializing the JSON file to objects"""
+         self.reload()
 
