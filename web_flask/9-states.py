bafafc0c46011cf8454c40a61d82bdf648a9e4b9
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-03-12 23:44:06 +0000

    Updated

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,61 +1,46 @@
-#!/usr/bin/python3
-"""
-Starts a Flask web application.
-
-This script starts a Flask web application that listens on 0.0.0.0, port 5000.
-It retrieves data from the storage engine (FileStorage or DBStorage) using the
-`storage` module from `models`.
-"""
-
 from flask import Flask, render_template
 from models import storage
 
 app = Flask(__name__)
-
 
 @app.route('/states', strict_slashes=False)
 def states():
     """
     Display a HTML page with all states.
 
-    This route handler retrieves all states from the storage and sorts them
-    alphabetically by name. Then, it renders a HTML template that displays
-    each state.
+    Retrieves all states from the storage and sorts them alphabetically by name.
+    Then, renders a HTML template that displays each state.
     """
-    states = storage.all("State").values()
-    states_sorted = sorted(states, key=lambda x: x.name)
-
-    return render_template('9-states.html', states=states_sorted)
-
+    states = storage.all('State').values()
+    states = sorted(states, key=lambda x: x.name)
+    return render_template('9-states.html', states=states)
 
 @app.route('/states/<state_id>', strict_slashes=False)
-def states_cities(state_id):
+def state(state_id):
     """
     Display a HTML page with cities of a specific state.
 
-    This route handler retrieves the state with the given id from the storage.
-    If the state is found, it renders a HTML template that displays the state
-    name and its associated cities.
-    If the state is not found, it renders a HTML template with "Not found!".
+    Retrieves the state with the given id from the storage.
+    If the state is found, renders a HTML template that displays the state name
+    and its associated cities. If the state is not found, returns a "Not found!"
+    message.
     """
-    state = storage.get("State", state_id)
+    state = storage.get('State', state_id)
     if state:
         cities = sorted(state.cities, key=lambda x: x.name)
-        return render_template('9-states.html', state=state, cities=cities)
+        return render_template('9-state.html', state=state, cities=cities)
     else:
-        return render_template('9-states.html', not_found=True)
-
+        return "Not found!"
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
+def teardown(exception):
     """
     Remove the current SQLAlchemy Session.
 
-    This function is called after each request to remove the current SQLAlchemy
-    session, preventing any potential memory leaks.
+    Called after each request to remove the current SQLAlchemy session,
+    preventing any potential memory leaks.
     """
     storage.close()
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
