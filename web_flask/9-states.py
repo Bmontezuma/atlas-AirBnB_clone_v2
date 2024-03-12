6d47a2295a0b18c43d12d276905d0994562e9f76
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-03-12 23:48:33 +0000

    9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,19 +1,23 @@
+#!/usr/bin/python3
 from flask import Flask, render_template
 from models import storage
 
 app = Flask(__name__)
+
 
 @app.route('/states', strict_slashes=False)
 def states():
     """
     Display a HTML page with all states.
 
-    Retrieves all states from the storage and sorts them alphabetically by name.
+    Retrieves all states from the storage and
+    sorts them alphabetically by name.
     Then, renders a HTML template that displays each state.
     """
     states = storage.all('State').values()
     states = sorted(states, key=lambda x: x.name)
     return render_template('9-states.html', states=states)
+
 
 @app.route('/states/<state_id>', strict_slashes=False)
 def state(state_id):
@@ -21,8 +25,10 @@ def state(state_id):
     Display a HTML page with cities of a specific state.
 
     Retrieves the state with the given id from the storage.
-    If the state is found, renders a HTML template that displays the state name
-    and its associated cities. If the state is not found, returns a "Not found!"
+    If the state is found, renders a HTML template that
+    displays the state name
+    and its associated cities. If the state is not found,
+    returns a "Not found!"
     message.
     """
     state = storage.get('State', state_id)
@@ -31,6 +37,7 @@ def state(state_id):
         return render_template('9-state.html', state=state, cities=cities)
     else:
         return "Not found!"
+
 
 @app.teardown_appcontext
 def teardown(exception):
@@ -41,6 +48,7 @@ def teardown(exception):
     preventing any potential memory leaks.
     """
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
